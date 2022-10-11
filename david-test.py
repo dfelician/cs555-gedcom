e3a159d63605f7f6e0788d0af40a3e282afa47d1
David <dfelician.97@gmail.com>
2022-10-10 20:14:12 -0400

    US 35 Test

---
+++ david-test.py
@@ -0,0 +1,27 @@
+import unittest
+from datetime import date
+import david
+import data
+
+
+class TestUS35(unittest.TestCase):
+    def test_lessThan30Days(self):
+        self.assertTrue(david.bornWithin30Days(date(2022, 9, 30)))
+
+    def test_moreThan30Days(self):
+        self.assertFalse(david.bornWithin30Days(date(2020, 10, 10)))
+
+    def test_correctNumberOfRecentBirths(self):
+        self.assertEqual(len(david.getRecentBirths()), 1)
+
+    def test_correctPerson(self):
+        self.assertEqual(david.getRecentBirths()[0].name, "Layla /Frick/")
+
+    def test_correctObject(self):
+        recentBirths = david.getRecentBirths()
+        for person in recentBirths:
+            self.assertEqual(type(person), data.Person)
+
+
+if __name__ == '__main__':
+    unittest.main()
